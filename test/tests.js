f5d63aa28f351e84953c9b2ec161f7b5b3200eaa
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-11-07 17:59:31 +0100

    Tested home petition

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,25 @@ var app = require('../app');
 var supertest = require('supertest');
 var assert = require('assert');
 
+function chk(err, done) {
+  if (err) {
+    console.log(err)
+    done()
+  }
+}
+
 describe('NoSQL´s API Test suite', function() {
-  console.log("-- BEGINNIG OF TESTS --")
+  console.log("-- BEGINNING OF TESTS --")
+  console.log("- MISCELANEOUS TESTS -")
+
+  it('GET Home "/"', (done) => {
+    supertest(app)
+    .get('/')
+    .expect(200)
+    .end(function(err, result) {
+      chk(err, done)
+      assert.equal(result.body.message, "Welcome to my NoSQL's API app!")
+      done()
+    })
+  })
 })
