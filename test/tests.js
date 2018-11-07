5a44bd7cd473f2dbd2e7619e14c52b7c8275b0d8
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-11-07 17:40:42 +0100

    Mocha tests enabled and command setted in package.json

---
+++ test/tests.js
@@ -0,0 +1,7 @@
+var app = require('../app');
+var supertest = require('supertest');
+var assert = require('assert');
+
+describe('NoSQL´s API Test suite', function() {
+  console.log("-- BEGINNIG OF TESTS --")
+})
